274b4d53fff71b4e3e5575110fa4618f7d3d35a9
p1105xmv <twins1228@gmail.com>
2024-06-11 20:58:27 +0800

    first create the project

---
+++ src/main/java/com/myProduct/MyFinancialProductApplication.java
@@ -0,0 +1,13 @@
+package com.myProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinancialProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFinancialProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myProduct/MyFinancialProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinancialProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
